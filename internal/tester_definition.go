9f1960144f6f7c890c2a8f6172e915fa9cc29e23
CodeCrafters (Bot) <hello@codecrafters.io>
2024-04-10 20:15:50 +0100

    Initial commit

---
+++ internal/tester_definition.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"github.com/codecrafters-io/tester-utils/tester_definition"
+)
+
+var testerDefinition = tester_definition.TesterDefinition{
+	AntiCheatTestCases: []tester_definition.TestCase{},
+	ExecutableFileName: "script.sh",
+	TestCases: []tester_definition.TestCase{
+		{
+			Slug:     "init",
+			TestFunc: testInit,
+		},
+	},
+}
